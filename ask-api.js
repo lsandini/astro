7f052ce86d3613679c1b8a191fda02674b2d9b56
Lorenzo <lsandini@gmail.com>
2021-11-29 21:14:56 +0200

    Structure

--- ask-api.js
+++ ask-api.js
@@ -17,7 +17,17 @@ async function fetchText() {
   let response = await fetch("https://astro6.herokuapp.com/", requestOptions);
   let data = await response.json();
   const dataJSON = JSON.stringify(data, null, 4);
+
+  const dataArray = Object.entries(data);
+  dataArray.forEach(([key, value]) => {
+    console.log(key); // 'one'
+    console.log(value); // 1
+  });
+  const arrayJSON = JSON.stringify(dataArray, null, 4);
   await writeFile("./data-local.json", dataJSON);
+  await writeFile("./data-array.json", arrayJSON);
   console.log(data);
+  //console.log(dataArray);
+  //console.log('dataArray[14]:',dataArray[14]);
 }
 fetchText();
